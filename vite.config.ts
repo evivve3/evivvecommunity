721cf53b542fadc96c911235036e04b29a011877
mohsinmem <mmemon@gamitar.com>
2023-02-18 17:39:49 +0530

    updated ui website

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 index: "./index.html",
+                map: "./map.html"
                 ...getMapsScripts("./maps"),
             },
         },
