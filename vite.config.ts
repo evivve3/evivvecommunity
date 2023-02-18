c6a92214e9680b656baf5193cde6ce411b9625ff
mohsinmem <mmemon@gamitar.com>
2023-02-18 17:43:57 +0530

    syntax fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 index: "./index.html",
-                map: "./map.html"
+                map: "./map.html",
                 ...getMapsScripts("./maps"),
             },
         },
