903e4b1cac0e5991d7831a14a39e508ceb49dc55
mohsinmem <mmemon@gamitar.com>
2023-04-01 12:46:27 +0530

    test with google.com as website embed

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ WA.onInit().then(() => {
     WA.room.area.onLeave('clock').subscribe(closePopup)
 
     const map: CreateUIWebsiteEvent = {
-        url:  "./map.png",
+        url:  "https://google.com",
         visible: true,
         allowApi: true,
         allowPolicy: "",   // The list of feature policies allowed
