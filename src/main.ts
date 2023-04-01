65d142c24233caa87ed3ef17c543e1f578fa17a5
mohsinmem <mmemon@gamitar.com>
2023-04-01 12:44:44 +0530

    updated minimap name

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ WA.onInit().then(() => {
     WA.room.area.onLeave('clock').subscribe(closePopup)
 
     const map: CreateUIWebsiteEvent = {
-        url:  "./map.html",
+        url:  "./map.png",
         visible: true,
         allowApi: true,
         allowPolicy: "",   // The list of feature policies allowed
